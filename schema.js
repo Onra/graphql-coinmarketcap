1ebb49a468b8adde7cfc5467be39de880ed8965a
Arnaud Christ <arnaud.christ@gmail.com>
2017-09-19 23:10:34 +0200

    Add all availables fields for a ticker from the original API.

--- schema.js
+++ schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLList,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLFloat
 } = require('graphql')
 
 const TickerType = new GraphQLObjectType({
@@ -19,6 +20,50 @@ const TickerType = new GraphQLObjectType({
         symbol: {
             type: GraphQLString,
             resolve: json => json.symbol
+        },
+        rank: {
+            type: GraphQLInt,
+            resolve: json => json.rank
+        },
+        priceUSD: {
+            type: GraphQLFloat,
+            resolve: json => json.price_usd
+        },
+        priceBTC: {
+            type: GraphQLFloat,
+            resolve: json => json.price_btc
+        },
+        dailyVolumeUSD: {
+            type: GraphQLFloat,
+            resolve: json => json["24h_volume_usd"]
+        },
+        marketCapUSD: {
+            type: GraphQLFloat,
+            resolve: json => json.market_cap_usd
+        },
+        availableSupply: {
+            type: GraphQLFloat,
+            resolve: json => json.available_supply
+        },
+        totalSupply: {
+            type: GraphQLFloat,
+            resolve: json => json.total_supply
+        },
+        percentChange1h: {
+            type: GraphQLFloat,
+            resolve: json => json.percent_change_1h
+        },
+        percentChange24h: {
+            type: GraphQLFloat,
+            resolve: json => json.percent_change_24h
+        },
+        percentChange7d: {
+            type: GraphQLFloat,
+            resolve: json => json.percent_change_7d
+        },
+        lastUpdated: {
+            type: GraphQLInt,
+            resolve: json => json.last_updated
         }
     }
 })
